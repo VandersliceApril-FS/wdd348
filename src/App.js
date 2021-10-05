d34a8de7a56ce3189de3835bf09b2ec1a15a0aae
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2021-10-05 12:57:50 -0500

    cleaned up some extra spaces

--- src/App.js
+++ src/App.js
@@ -22,9 +22,3 @@ function App() {
 }
 
 export default App;
-
-
-  //   fetch("http://openlibrary.org/search.json?title=on+writing_+well&fields=*,availability&limit=3")
-  //   .then(response => response.json())
-  //   .then(setData);
-  // }, []);
