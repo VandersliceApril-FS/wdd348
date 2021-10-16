5f3bbab6ed76f05d789039dda3034e8ee0604668
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2021-10-16 14:48:02 -0500

    Started designing the main/home page. Ref #9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+
+
+const styles = {
+    heading: {
+        width: '30rem',
+        height: '25rem',
+        fontSize: '4.5rem',
+        
+    }
+}
 
 function Home() {
     return (
-        <div>
-            <p>This app helps keep track of books in your personal library. Add titles of digital and physical books</p>
-        </div>
+        <Container style={styles.bg}>
+            <div>
+                <h1 style={styles.heading}><strong>Keep track</strong> of all books in your library</h1>
+            </div>
+            
+        </Container>
     )
 }
 
