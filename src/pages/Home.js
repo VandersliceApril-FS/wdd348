82fdb2cb6d30a2b2aeac3a0b1b43961c1467f736
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2021-10-22 16:35:57 -0500

    fixed the styling for mobile view in landing page. Closes #14.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Container, Button } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import HeroImage from '../images/ereader.jpg'
 
 const styles = {
-    container: {
+    headingContainer: {
+        margin: '0 auto',
         display: 'flex',
-        justifyContent: 'space-around',
-    },
-    hero: {
-        height: '35rem'
+        justifyContent: 'center',
+        width: '70%'
     },
     heading: {
         marginBottom: '2rem',
@@ -26,27 +26,18 @@ const styles = {
 
 function Home() {
     return (
-        <Container>
-            <div style={styles.headingContainer}>
-                <h1 style={styles.heading}><strong>Keep track</strong> of all books in your library</h1>
-                <p>We currently live in a sort of limbo where some of our books live on the shelf and some live in a device. Keep track of both digital and physical books by cataloguing them in home library.</p>
-                <NavLink 
+        <section className="homeContainer">
+            <div className="aboutContainer">
+                <h1 className="homeHeader"><strong>Keep track</strong> of all books in your library</h1>
+                <p className="homeAbout">We currently live in a sort of limbo where some of our books live on the shelf and some live in a device. Keep track of both digital and physical books by cataloguing them in home library.</p>
+            </div>
+            <NavLink 
                 style={styles.link}
                 to='/SearchBooks'
-                activeStyle={{
-                  fontWeight: 'bold',
-                  borderBottom: '1px solid #764a6d',
-                  color: '#A68A64'
-                }}>
-                    <Button style={styles.button}>Start Adding Books</Button>
-                </NavLink>  
-                
-            </div>
-            {/* <div style={styles.heroContainer}>
-                <img style={styles.hero} src={HeroImage} alt="" />
-            </div> */}
-            
-        </Container>
+                >
+                    <Button className="buttonPrimary">Start Adding Books</Button>
+                </NavLink> 
+        </section>
     )
 }
 
